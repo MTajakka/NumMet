068a0114d8c096b53a696bea9afc484fcf83e701
Markus Tajakka <mk.tajakka@gmail.com>
2026-02-03 17:24:54 +0200

    Tendency equations

--- shallow_water.py
+++ shallow_water.py
@@ -65,68 +65,157 @@ def meshgrid(dims: Union[Sequence[int], int],
     axis = [np.arange(0, i_dim, 1) * dx for i_dim, dx in zip(dims, d_dims)]
     return np.meshgrid(*axis)
 
-def roll_diff(x: npt.ArrayLike,
+def index_on_axis(i, axis: int, n_dim: int):
+    if axis > n_dim:
+        raise ValueError('axis out of range')
+    # s = (slice(None),)* n_dim
+    # s[axis] = i
+    return (slice(None),)*axis + (i,) + (slice(None),)*(n_dim-axis-1)
+
+def shape_normal(shape: Tuple[int, ...], axis: int) -> Tuple[int, ...]:
+    if axis > len(shape):
+        raise ValueError('axis out of range')
+    return shape[:axis] + shape[axis+1:]
+
+def roll_diff(x: np.ndarray,
               cyclic: bool=False,
-              axis: int=0) -> np.ndarray:
+              axis: int=0,
+              boundary: Union[Tuple[np.ndarray, np.ndarray], float]=0.0
+              ) -> np.ndarray:
+    
     x_left = np.roll(x, 1, axis=axis)
     x_right = np.roll(x, -1, axis=axis)
+    
     if not cyclic:
-        x_left[-1] = 0
-        x_right[0] = 0
+        boundary_shape = shape_normal(x.shape, axis)
+        
+        if np.isscalar(boundary):
+            boundary = np.full((2,) + boundary_shape, boundary)
+        elif boundary[0].shape != boundary[1].shape:
+            raise ValueError('Boundary shapes do not match')
+        elif boundary[0].shape != boundary_shape:
+            raise ValueError('Given boundary doesnt match shape normal to axis')
+        
+        n_dim = len(x.shape)
+        x_left[index_on_axis(0, axis, n_dim)] = boundary[0]
+        x_right[index_on_axis(-1, axis, n_dim)] = boundary[1]
         
     return 1/2 * (x_right - x_left)
 
 # %% Shallow Water model
 
-
 class ShallowWater:
     DT: float
-    D_DIM: Sequence[float]
-    h: np.ndarray
-    u: np.ndarray
-    v: np.ndarray
-    CYCLIC: Sequence[bool]
+    DX: float
+    DY: float
+    G: float
+    now: np.ndarray
+    old: np.ndarray
+    time: np.ndarray
+    AXIS_X: int
+    AXIS_Y: int
+    
+    CYCLIC_X: bool
+    CYCLIC_Y: bool
     N_DIMS: int
     
     F: np.ndarray
     H_MEAN: float
     U_MEAN: float
     
-    t_h: np.ndarray
-    t_u: np.ndarray
-    t_v: np.ndarray
-    
-    
     def __init__(self, 
-                 dt: float,
                  h: npt.ArrayLike,
+                 dt: float,
                  d_dim: Union[Sequence[float], float],
                  cyclic: Union[Sequence[bool], bool],
+                 g: float,
                  h_mean: float,
-                 u_mean: float,
+                 u_mean: float=10,
                  ):
+        
         self.DT = dt
-        self.h = h
-        self.u = np.zeros_like(h)
-        self.v = np.zeros_like(h)
+        self.H_MEAN = h_mean
+        self.U_MEAN = u_mean
+        self.G = g
+        
+        self.now = np.zeros(h.shape, dtype=[('h','d'), ('u','d'), ('v','d')])
+        self.now['h'] = h
         self.F = np.zeros_like(h)
+        
         self.N_DIMS = len(np.shape(h))
         if self.N_DIMS > 2:
             raise NotImplementedError('There isnt a shallow water model for '
                                       + 'higer than two dimensions')
         
+        if self.N_DIMS == 1:
+            self.AXIS_X = 0,
+            self.AXIS_Y = None
+        else:
+            self.AXIS_X = 1
+            self.AXIS_Y = 0
+        
         if not isinstance(d_dim, Sequence):
             d_dim = (d_dim) * self.N_DIMS
-        self.D_DIM = d_dim
-        if len(self.D_DIM) != self.N_DIMS:
+        if len(d_dim) != self.N_DIMS:
             raise ValueError('Dimensions of d_dim does not match models own')
+        self.DX = d_dim[0]
+        self.DY = d_dim[1]
         if not isinstance(cyclic, Sequence):
             cyclic = (cyclic) * self.N_DIMS
-        self.CYCLIC = cyclic
-        if len(self.CYCLIC) != self.N_DIMS:
+        if len(cyclic) != self.N_DIMS:
             raise ValueError('Dimensions of cyclic does not match models own')
+        self.CYCLIC_X = cyclic[0]
+        self.CYCLIC_Y = cyclic[1]
         
         self.initialize_time()
+    
+    def shape(self):
+        return self.now.shape
+    
+    def _1D_slope(self):
+        return -self.F / self.G * self.U_MEAN * self.DY
+    
+    def _zeros(self):
+        return np.zeros(self.shape(), dtype='d')
+    
+    def _u_tendency(self):
+        u_tend = self.zeros()
+        
+        du_x = roll_diff(self.now['u'], axis=self.AXIS_X, cyclic=True)
+        u_tend -= self.now['u'] * du_x / self.DX
+        
+        u_tend -= -self.F * self.now['v']
+        
+        dh_x = roll_diff(self.now['h'], axis=self.AXIS_X, cyclic=True)
+        u_tend -= self.G * dh_x / self.DX
+        
+        return u_tend
+        
+    def _v_tendency(self):
+        v_tend = self.zeros()
+        
+        dv_x = roll_diff(self.now['v'], axis=self.AXIS_X, cyclic=True)
+        v_tend -= self.now['u'] * dv_x / self.DX
+        
+        v_tend -= self.F * self.now['u']
+        
+        dh_y = self._1D_slope()
+        v_tend -= self.G * dh_y / self.DY
+        
+        return v_tend
+    
+    def _h_tendency(self):
+        h_tend = self.zeros()
+        
+        dh_x = roll_diff(self.now['h'], axis=self.AXIS_X, cyclic=True)
+        h_tend -= self.now['u'] * dh_x / self.DX
+        
+        dh_y = self._1D_slope()
+        h_tend -= self.now['v'] * dh_y / self.DY
+        
+    def _tendency(self):
+        pass
+        
         
     def euler_step(self):
         pass
@@ -135,8 +224,5 @@ class ShallowWater:
         pass
     
     def initialize_time(self):
-        self.t_h = np.array([self.h[:]])
-        self.t_u = np.array([self.u[:]])
-        self.t_v = np.array([self.v[:]])
-        
+        self.time = np.array([self.now[:]])
         self.euler_step()
